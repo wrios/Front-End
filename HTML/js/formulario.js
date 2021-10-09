a8c3dc21bfeabba2669d4e990a79fa4dd3b24023
wrios <kennedy.wrc@hotmail.com>
2021-10-08 21:43:40 -0300

    funcionalidad de formulario
    nombre, apellido, email

--- HTML/js/formulario.js
+++ HTML/js/formulario.js
@@ -1,8 +1,29 @@
-function clic(){
-    var mascota = document.getElementById("mascota").value;
-    if (mascota = "") {
-        alert("Debes ingresar un nombre de mascota");
-    }else{
-        alert(`Buen nobre: ${mascota}`);
+function validarEmail(email) {
+    var primerAparicion = email.indexOf("@");
+    var segundaAparicion = email.lastIndexOf("@");
+    if (primerAparicion != segundaAparicion || primerAparicion == -1) {
+        return "invalido";
     }
+    var ultimoPunto = email.lastIndexOf(".");
+    if (ultimoPunto <= primerAparicion) {
+        return "invalido";
+    }
+    return "";
+}
+function validarForm(form){
+    var error = document.getElementById("error");
+    var nombre = form.elements["nombre"].value;
+    var apellido = form.elements["apellido"].value;
+    var email = form.elements["email"].value;
+    var mensajesError = [];
+    if (nombre == null || nombre == '') {   
+        mensajesError.push('Ingresa tu nombre');
+    }
+    if (apellido == '' || apellido == null) {
+        mensajesError.push('Ingresa tu apellido');
+    }
+    if (validarEmail(email) == "invalido") {
+        mensajesError.push("email inválido");
+    }
+    error.innerHTML = mensajesError.join(",");
 }
